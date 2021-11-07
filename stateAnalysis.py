f968b60f0d1213a70c3961a1d940eac1f9e670b2
kubiteddie <71346474+kubiteddie@users.noreply.github.com>
2021-11-06 20:05:08 -0400

    States

--- stateAnalysis.py
+++ stateAnalysis.py
@@ -42,14 +42,30 @@ for i in range(1,len(rows)):
         stateNewSum+=int(rows[i][17])
         stateTotalSum+=int(rows[i][18])
 
-emergingMarkets = []
-slowingMarkets = []
 for i in range(0, len(stateAndSums)):
     ratio = 1.0*stateAndSums[i][1]/stateAndSums[i][2]
-    stateAndSums
+    stateAndSums[i].append(ratio)
 
+stateAndSums.sort(key = lambda x : x[3])
+lowerBound = math.floor(len(stateAndSums) * .6)
+upperBound = math.ceil(len(stateAndSums) * .8)
 
-print(emergingMarkets)
-print(slowingMarkets)
+emergingMarkets = stateAndSums[lowerBound:upperBound]
+maxRatio = stateAndSums[len(stateAndSums)-1][3]
+for i in range(0, len(emergingMarkets)):
+    proj = emergingMarkets[i][2] * maxRatio
+    emergingMarkets[i].append(proj)
+    target = proj - emergingMarkets[i][1]
+    emergingMarkets.append(target)
+
+#JSON output includes the market classification as emerging, the lower bound of market percentiles, the upper bound
+#of the same thing, and the maximum ratio of new prescriptions to total prescriptions. Also included are the states
+#that are emerging markets. The state objects contain data with the name of the state, the current new prescriptions
+#over 6 months, the current total prescriptions over 6 months, the ratio of new to total, the projected new 
+#prescriptions, and the target of new prescriptions. The projection is calculated by multiplying the current total
+#prescriptions by the maximum ratio, essentially maximizing new prescriptions per state. The target is then 
+#calculated by subtracting the current prescriptions from the projection, giving a change in prescriptions, thus
+#representing the target increase
+
 
 
